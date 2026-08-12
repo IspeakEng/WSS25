213bc4dac49ff21ba665bbab422cb67993d59d75
minhajo-dw <minhazulislamnew@gmail.com>
2026-08-12 12:27:06 +0600

    Change timestamp format to ISO string in afk.js

--- src/commands/Utility/afk.js
+++ src/commands/Utility/afk.js
@@ -83,7 +83,7 @@ export default {
                     key,
                     {
                         reason: reason,
-                        timestamp: Date.now(),
+                       timestamp: new Date().toISOString(),
                     }
                 );
 
